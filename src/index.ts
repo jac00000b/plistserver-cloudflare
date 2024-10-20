24bf1be8d12e9cac392d26a6ac2eb6812ffbe90a
jac00000b <118233742+jac00000b@users.noreply.github.com>
2024-10-20 22:03:50 +0800

    add genPlist

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,48 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from "hono";
+const app = new Hono();
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+const PLIST_TEMPLATE = `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>items</key>
+    <array>
+        <dict>
+            <key>assets</key>
+            <array>
+                <dict>
+                    <key>kind</key>
+                    <string>software-package</string>
+                    <key>url</key>
+                    <string>{fetchurl}</string>
+                </dict>
+            </array>
+            <key>metadata</key>
+            <dict>
+                <key>bundle-identifier</key>
+                <string>{bundleid}</string>
+                <key>bundle-version</key>
+                <string>{version}</string>
+                <key>kind</key>
+                <string>software</string>
+                <key>title</key>
+                <string>{name}</string>
+            </dict>
+        </dict>
+    </array>
+</dict>
+</plist>`;
+
+app.get("/genPlist", (c) => {
+  const { bundleid, version, name, fetchurl } = c.req.query();
+  const plist = PLIST_TEMPLATE.replace("{bundleid}", bundleid)
+    .replace("{version}", version)
+    .replace("{name}", name)
+    .replace("{fetchurl}", fetchurl);
+
+  return c.text(plist, 200, {
+    "Content-Type": "application/octet-stream",
+  });
+});
+
+export default app;
